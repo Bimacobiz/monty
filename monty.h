c43441d6a5c61d7f369c0822ec630c7b5e865227
castykawira <murithicasty@gmail.com>
2023-08-23 15:32:19 -0700

    new changes

--- monty.h
+++ monty.h
@@ -6,16 +6,16 @@
 #include <unistd.h>
 
 /**
-* struct stack_s - Structure to represent each element in the stack
-* @n: Integer value stored in the node
-* @prev: Pointer to the previous element
-* @next: Pointer to the next element
-*/	
+ * struct stack_s - Structure to represent each element in the stack
+ * @n: Integer value stored in the node
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
+ */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 int main(int arg_count, char *arg_vec[]);
 void pall(stack_t *top);
